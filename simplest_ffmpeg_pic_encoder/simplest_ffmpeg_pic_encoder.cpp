2aace64705a52a86dd12a7ddc2437ae46c0c6a70
leixiaohua1020 <leixiaohua1020@126.com>
2014-06-05 19:36:15 +0800

    leixiaohua1020 commit

---
+++ simplest_ffmpeg_pic_encoder/simplest_ffmpeg_pic_encoder.cpp
@@ -0,0 +1,144 @@
+/* 
+ *最简单的基于FFmpeg的图像编码器
+ *Simplest FFmpeg Picture Encoder
+ *
+ *雷霄骅 Lei Xiaohua
+ *leixiaohua1020@126.com
+ *中国传媒大学/数字电视技术
+ *Communication University of China / Digital TV Technology
+ *http://blog.csdn.net/leixiaohua1020
+ *
+ *本程序实现了YUV420P像素数据编码为JPEG图片。是最简单的FFmpeg编码方面的教程。
+ *通过学习本例子可以了解FFmpeg的编码流程。
+ */
+
+#include "stdafx.h"
+
+extern "C"
+{
+#include <libavcodec\avcodec.h>
+#include <libavformat\avformat.h>
+#include <libswscale\swscale.h>
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	AVFormatContext* pFormatCtx;
+	AVOutputFormat* fmt;
+	AVStream* video_st;
+	AVCodecContext* pCodecCtx;
+	AVCodec* pCodec;
+
+	uint8_t* picture_buf;
+	AVFrame* picture;
+	int size;
+
+	FILE *in_file = fopen("cuc_view_480x272.yuv", "rb");	//视频YUV源文件 
+	int in_w=480,in_h=272;									//宽高
+	const char* out_file = "cuc_view_encode.jpg";					//输出文件路径
+
+	av_register_all();
+
+	//方法1.组合使用几个函数
+	pFormatCtx = avformat_alloc_context();
+	//猜格式。用MJPEG编码
+	fmt = av_guess_format("mjpeg", NULL, NULL);
+	pFormatCtx->oformat = fmt;
+	//注意：输出路径
+	if (avio_open(&pFormatCtx->pb,out_file, AVIO_FLAG_READ_WRITE) < 0)
+	{
+		printf("输出文件打开失败");
+		return -1;
+	}
+
+	//方法2.更加自动化一些
+	//avformat_alloc_output_context2(&pFormatCtx, NULL, NULL, out_file);
+	//fmt = pFormatCtx->oformat;
+
+	video_st = av_new_stream(pFormatCtx, 0);
+	if (video_st==NULL)
+	{
+		return -1;
+	}
+	pCodecCtx = video_st->codec;
+	pCodecCtx->codec_id = fmt->video_codec;
+	pCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
+	pCodecCtx->pix_fmt = PIX_FMT_YUVJ420P;
+
+	pCodecCtx->width = in_w;  
+	pCodecCtx->height = in_h;
+
+	pCodecCtx->time_base.num = 1;  
+	pCodecCtx->time_base.den = 25;   
+	//输出格式信息
+	av_dump_format(pFormatCtx, 0, out_file, 1);
+
+	pCodec = avcodec_find_encoder(pCodecCtx->codec_id);
+	if (!pCodec)
+	{
+		printf("没有找到合适的编码器！");
+		return -1;
+	}
+	if (avcodec_open2(pCodecCtx, pCodec,NULL) < 0)
+	{
+		printf("编码器打开失败！");
+		return -1;
+	}
+	picture = avcodec_alloc_frame();
+	size = avpicture_get_size(pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
+	picture_buf = (uint8_t *)av_malloc(size);
+	if (!picture_buf)
+	{
+		return -1;
+	}
+	avpicture_fill((AVPicture *)picture, picture_buf, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
+
+	//写文件头
+	avformat_write_header(pFormatCtx,NULL);
+
+	AVPacket pkt;
+	int y_size = pCodecCtx->width * pCodecCtx->height;
+	av_new_packet(&pkt,y_size*3);
+	//读入YUV
+	if (fread(picture_buf, 1, y_size*3/2, in_file) < 0)
+	{
+		printf("文件读取错误");
+		return -1;
+	}
+	picture->data[0] = picture_buf;  // 亮度Y
+	picture->data[1] = picture_buf+ y_size;  // U 
+	picture->data[2] = picture_buf+ y_size*5/4; // V
+	int got_picture=0;
+	//编码
+	int ret = avcodec_encode_video2(pCodecCtx, &pkt,picture, &got_picture);
+	if(ret < 0)
+	{
+		printf("编码错误！\n");
+		return -1;
+	}
+	if (got_picture==1)
+	{
+		pkt.stream_index = video_st->index;
+		ret = av_write_frame(pFormatCtx, &pkt);
+	}
+
+	av_free_packet(&pkt);
+	//写文件尾
+	av_write_trailer(pFormatCtx);
+
+	printf("编码成功！\n");
+
+	if (video_st)
+	{
+		avcodec_close(video_st->codec);
+		av_free(picture);
+		av_free(picture_buf);
+	}
+	avio_close(pFormatCtx->pb);
+	avformat_free_context(pFormatCtx);
+
+	fclose(in_file);
+
+	return 0;
+}
+
