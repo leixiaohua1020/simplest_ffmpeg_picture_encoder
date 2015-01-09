9eca0319cbe63a68ced8f7d57441b6fd269e5b68
unknown <leixiaohua1020@126.com>
2015-01-09 17:07:12 +0800

    remove stdafx.h

--- simplest_ffmpeg_pic_encoder/simplest_ffmpeg_pic_encoder.cpp
+++ simplest_ffmpeg_pic_encoder/simplest_ffmpeg_pic_encoder.cpp
@@ -1,27 +1,27 @@
-/* 
- *最简单的基于FFmpeg的图像编码器
- *Simplest FFmpeg Picture Encoder
- *
- *雷霄骅 Lei Xiaohua
- *leixiaohua1020@126.com
- *中国传媒大学/数字电视技术
- *Communication University of China / Digital TV Technology
- *http://blog.csdn.net/leixiaohua1020
- *
- *本程序实现了YUV420P像素数据编码为JPEG图片。是最简单的FFmpeg编码方面的教程。
- *通过学习本例子可以了解FFmpeg的编码流程。
+/**
+ * 最简单的基于FFmpeg的图像编码器
+ * Simplest FFmpeg Picture Encoder
+ * 
+ * 雷霄骅 Lei Xiaohua
+ * leixiaohua1020@126.com
+ * 中国传媒大学/数字电视技术
+ * Communication University of China / Digital TV Technology
+ * http://blog.csdn.net/leixiaohua1020
+ * 
+ * 本程序实现了YUV420P像素数据编码为JPEG图片。是最简单的FFmpeg编码方面的教程。
+ * 通过学习本例子可以了解FFmpeg的编码流程。
  */
 
-#include "stdafx.h"
+#include <stdio.h>
 
 extern "C"
 {
-#include <libavcodec\avcodec.h>
-#include <libavformat\avformat.h>
-#include <libswscale\swscale.h>
+#include "libavcodec\avcodec.h"
+#include "libavformat\avformat.h"
+#include "libswscale\swscale.h"
 };
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	AVFormatContext* pFormatCtx;
 	AVOutputFormat* fmt;
@@ -31,11 +31,18 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	uint8_t* picture_buf;
 	AVFrame* picture;
+	AVPacket pkt;
+	int y_size;
+	int got_picture=0;
 	int size;
 
-	FILE *in_file = fopen("cuc_view_480x272.yuv", "rb");	//视频YUV源文件 
+	int ret=0;
+
+	FILE *in_file = NULL;	//视频YUV源文件 
 	int in_w=480,in_h=272;									//宽高
 	const char* out_file = "cuc_view_encode.jpg";					//输出文件路径
+
+	in_file = fopen("cuc_view_480x272.yuv", "rb");
 
 	av_register_all();
 
@@ -45,8 +52,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	fmt = av_guess_format("mjpeg", NULL, NULL);
 	pFormatCtx->oformat = fmt;
 	//注意：输出路径
-	if (avio_open(&pFormatCtx->pb,out_file, AVIO_FLAG_READ_WRITE) < 0)
-	{
+	if (avio_open(&pFormatCtx->pb,out_file, AVIO_FLAG_READ_WRITE) < 0){
 		printf("输出文件打开失败");
 		return -1;
 	}
@@ -56,8 +62,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	//fmt = pFormatCtx->oformat;
 
 	video_st = av_new_stream(pFormatCtx, 0);
-	if (video_st==NULL)
-	{
+	if (video_st==NULL){
 		return -1;
 	}
 	pCodecCtx = video_st->codec;
@@ -74,13 +79,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	av_dump_format(pFormatCtx, 0, out_file, 1);
 
 	pCodec = avcodec_find_encoder(pCodecCtx->codec_id);
-	if (!pCodec)
-	{
+	if (!pCodec){
 		printf("没有找到合适的编码器！");
 		return -1;
 	}
-	if (avcodec_open2(pCodecCtx, pCodec,NULL) < 0)
-	{
+	if (avcodec_open2(pCodecCtx, pCodec,NULL) < 0){
 		printf("编码器打开失败！");
 		return -1;
 	}
@@ -96,8 +99,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	//写文件头
 	avformat_write_header(pFormatCtx,NULL);
 
-	AVPacket pkt;
-	int y_size = pCodecCtx->width * pCodecCtx->height;
+	y_size = pCodecCtx->width * pCodecCtx->height;
 	av_new_packet(&pkt,y_size*3);
 	//读入YUV
 	if (fread(picture_buf, 1, y_size*3/2, in_file) < 0)
@@ -108,16 +110,14 @@ int _tmain(int argc, _TCHAR* argv[])
 	picture->data[0] = picture_buf;  // 亮度Y
 	picture->data[1] = picture_buf+ y_size;  // U 
 	picture->data[2] = picture_buf+ y_size*5/4; // V
-	int got_picture=0;
+
 	//编码
-	int ret = avcodec_encode_video2(pCodecCtx, &pkt,picture, &got_picture);
-	if(ret < 0)
-	{
+	ret = avcodec_encode_video2(pCodecCtx, &pkt,picture, &got_picture);
+	if(ret < 0){
 		printf("编码错误！\n");
 		return -1;
 	}
-	if (got_picture==1)
-	{
+	if (got_picture==1){
 		pkt.stream_index = video_st->index;
 		ret = av_write_frame(pFormatCtx, &pkt);
 	}
@@ -128,8 +128,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	printf("编码成功！\n");
 
-	if (video_st)
-	{
+	if (video_st){
 		avcodec_close(video_st->codec);
 		av_free(picture);
 		av_free(picture_buf);
