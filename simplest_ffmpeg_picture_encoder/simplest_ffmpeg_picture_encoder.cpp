67fbf37da9f106874d518c41daf74f11eb5592a1
unknown <leixiaohua1020@126.com>
2015-02-05 15:04:59 +0800

    compilecmd

--- simplest_ffmpeg_picture_encoder/simplest_ffmpeg_picture_encoder.cpp
+++ simplest_ffmpeg_picture_encoder/simplest_ffmpeg_picture_encoder.cpp
@@ -14,12 +14,30 @@
 
 #include <stdio.h>
 
+#define __STDC_CONSTANT_MACROS
+
+#ifdef _WIN32
+//Windows
 extern "C"
 {
 #include "libavcodec\avcodec.h"
 #include "libavformat\avformat.h"
 #include "libswscale\swscale.h"
 };
+#else
+//Linux...
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <libavcodec\avcodec.h>
+#include <libavformat\avformat.h>
+#include <libswscale\swscale.h>
+#ifdef __cplusplus
+};
+#endif
+#endif
+
 
 int main(int argc, char* argv[])
 {
