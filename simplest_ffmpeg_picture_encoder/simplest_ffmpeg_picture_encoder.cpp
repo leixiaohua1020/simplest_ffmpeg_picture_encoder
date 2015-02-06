c1d04b09beacd6e75e1711e3e4137d6e17f4c732
雷霄骅 <leixiaohua1020@126.com>
2015-02-06 16:17:25 +0800

    supportmac

--- simplest_ffmpeg_picture_encoder/simplest_ffmpeg_picture_encoder.cpp
+++ simplest_ffmpeg_picture_encoder/simplest_ffmpeg_picture_encoder.cpp
@@ -20,9 +20,8 @@
 //Windows
 extern "C"
 {
-#include "libavcodec\avcodec.h"
-#include "libavformat\avformat.h"
-#include "libswscale\swscale.h"
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
 };
 #else
 //Linux...
@@ -30,9 +29,8 @@ extern "C"
 extern "C"
 {
 #endif
-#include <libavcodec\avcodec.h>
-#include <libavformat\avformat.h>
-#include <libswscale\swscale.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 #ifdef __cplusplus
 };
 #endif
@@ -56,30 +54,30 @@ int main(int argc, char* argv[])
 
 	int ret=0;
 
-	FILE *in_file = NULL;	//视频YUV源文件 
-	int in_w=480,in_h=272;									//宽高
-	const char* out_file = "cuc_view_encode.jpg";					//输出文件路径
+	FILE *in_file = NULL;                            //YUV source
+	int in_w=480,in_h=272;                           //YUV's width and height
+	const char* out_file = "cuc_view_encode.jpg";    //Output file
 
 	in_file = fopen("cuc_view_480x272.yuv", "rb");
 
 	av_register_all();
 
-	//方法1.组合使用几个函数
+	//Method 1
 	pFormatCtx = avformat_alloc_context();
-	//猜格式。用MJPEG编码
+	//Guess format
 	fmt = av_guess_format("mjpeg", NULL, NULL);
 	pFormatCtx->oformat = fmt;
-	//注意：输出路径
+    //Output URL
 	if (avio_open(&pFormatCtx->pb,out_file, AVIO_FLAG_READ_WRITE) < 0){
-		printf("输出文件打开失败");
+		printf("Couldn't open output file.");
 		return -1;
 	}
 
-	//方法2.更加自动化一些
+	//Method 2. More simple
 	//avformat_alloc_output_context2(&pFormatCtx, NULL, NULL, out_file);
 	//fmt = pFormatCtx->oformat;
 
-	video_st = av_new_stream(pFormatCtx, 0);
+	video_st = avformat_new_stream(pFormatCtx, 0);
 	if (video_st==NULL){
 		return -1;
 	}
@@ -93,19 +91,19 @@ int main(int argc, char* argv[])
 
 	pCodecCtx->time_base.num = 1;  
 	pCodecCtx->time_base.den = 25;   
-	//输出格式信息
+	//Output some information
 	av_dump_format(pFormatCtx, 0, out_file, 1);
 
 	pCodec = avcodec_find_encoder(pCodecCtx->codec_id);
 	if (!pCodec){
-		printf("没有找到合适的编码器！");
+		printf("Codec not found.");
 		return -1;
 	}
 	if (avcodec_open2(pCodecCtx, pCodec,NULL) < 0){
-		printf("编码器打开失败！");
+		printf("Could not open codec.");
 		return -1;
 	}
-	picture = avcodec_alloc_frame();
+	picture = av_frame_alloc();
 	size = avpicture_get_size(pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
 	picture_buf = (uint8_t *)av_malloc(size);
 	if (!picture_buf)
@@ -114,25 +112,25 @@ int main(int argc, char* argv[])
 	}
 	avpicture_fill((AVPicture *)picture, picture_buf, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
 
-	//写文件头
+	//Write Header
 	avformat_write_header(pFormatCtx,NULL);
 
 	y_size = pCodecCtx->width * pCodecCtx->height;
 	av_new_packet(&pkt,y_size*3);
-	//读入YUV
-	if (fread(picture_buf, 1, y_size*3/2, in_file) < 0)
+	//Read YUV
+	if (fread(picture_buf, 1, y_size*3/2, in_file) <=0)
 	{
-		printf("文件读取错误");
+		printf("Could not read input file.");
 		return -1;
 	}
 	picture->data[0] = picture_buf;  // 亮度Y
 	picture->data[1] = picture_buf+ y_size;  // U 
 	picture->data[2] = picture_buf+ y_size*5/4; // V
 
-	//编码
+	//Encode
 	ret = avcodec_encode_video2(pCodecCtx, &pkt,picture, &got_picture);
 	if(ret < 0){
-		printf("编码错误！\n");
+		printf("Encode Error.\n");
 		return -1;
 	}
 	if (got_picture==1){
@@ -141,10 +139,10 @@ int main(int argc, char* argv[])
 	}
 
 	av_free_packet(&pkt);
-	//写文件尾
+	//Write Trailer
 	av_write_trailer(pFormatCtx);
 
-	printf("编码成功！\n");
+	printf("Encode Successful.\n");
 
 	if (video_st){
 		avcodec_close(video_st->codec);
